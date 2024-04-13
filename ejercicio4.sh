8903cca43eac964cd415a06634239a11bea87df5
Martìn Leandro Fallas Jimènez <leandro543fj@gmail.com>
2024-04-13 13:49:12 -0600

    Funciona correctamente

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -18,22 +18,6 @@ monitoriar(){
 
 main(){
 
-#      cd /etc/systemd/system/
-
-#      creationinfo="[Unit]
-# Description=Servicio de Monitoreo de Cambios en Directorios
-# After=network.target
-
-# [Service]
-# Type=simple
-# ExecStart="$(realpath ejercicio4.sh)"
-# WorkingDirectory="$(realpath Lab_2_plataformas)"
-
-# [Install]
-# WantedBy=multi-user.target"
-
-#     sudo echo $creationinfo > monitoriate.service
-
     monitoriar
     
 }
